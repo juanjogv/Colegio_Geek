e194f85489cdb8b765d9017b1d88260071fb54ad
JuanPabloRiveraAlvarezINEM <juanpariveraalvarez@gmail.com>
2021-01-28 17:53:06 -0500

    Agregamos tablas y corregimos errores

--- SQL/base_datos.sql
+++ SQL/base_datos.sql
@@ -2,12 +2,12 @@ CREATE DATABASE academia_geek;
 
 CREATE TYPE TIPO_DOCUMENTO AS ENUM('TI','NUIM','CC');
 CREATE TABLE usuarios(
-  id_usuarios BIGINT NOT NULL PRIMARY KEY,
+  id_usuario serial NOT NULL PRIMARY KEY,
   codigo_usuario varchar(10),
   tipo_documento TIPO_DOCUMENTO,
   documento_usuario varchar(15),
-  nombres_usuario varchar(30),
-  apellidos_usuario varchar(30),
+  nombre_usuario varchar(30),
+  apellido_usuario varchar(30),
   sexo BIGINT,
   fecha_nacimiento date,
   direccion_residencia varchar(20),
@@ -19,34 +19,37 @@ CREATE TABLE usuarios(
   copia_documento varchar(500));
 
 
+#--------------------------------------------------------------------------------------------------#
 
 CREATE TYPE JORNADA AS ENUM('mañana','tarde');
 CREATE TABLE grupos(
-  id_grupo BIGINT NOT NULL PRIMARY KEY,
+  id_grupo serial NOT NULL PRIMARY KEY,
   codigo_grupo varchar(10),
   jornada JORNADA);
 
 
+#--------------------------------------------------------------------------------------------------#
 
 CREATE TABLE usuarios_grupos(
-    id_usuarios_grupos BIGINT NOT NULL PRIMARY KEY,
-    id_usuarios BIGINT NOT NULL,
-    id_grupos BIGINT NOT NULL);
+    id_usuarios_grupo serial NOT NULL PRIMARY KEY,
+    id_usuario BIGINT NOT NULL,
+    id_grupo BIGINT NOT NULL);
 
-  ALTER TABLE usuarios_grupos
-	ADD CONSTRAINT fk_grupos
-	FOREIGN KEY (id_grupos)
+ALTER TABLE usuarios_grupos
+	ADD CONSTRAINT fk_grupo
+	FOREIGN KEY (id_grupo)
 	REFERENCES grupos (id_grupo);       
 
 ALTER TABLE usuarios_grupos
   ADD CONSTRAINT fk_usuarios_grupos_u
-  FOREIGN KEY (id_usuarios)
-  REFERENCES usuarios (id_usuarios)
+  FOREIGN KEY (id_usuario)
+  REFERENCES usuarios (id_usuario)
 
 
+#--------------------------------------------------------------------------------------------------#
 
 CREATE TABLE materias(
-  id_materia BIGINT NOT NULL PRIMARY KEY,
+  id_materia serial NOT NULL PRIMARY KEY,
   codigo_materia varchar(10),
   nombre_materia varchar(20),
   sexto SMALLINT,
@@ -64,14 +67,37 @@ ALTER TABLE materias
   REFERENCES grupos (id_grupo); 
 
 
-
+#--------------------------------------------------------------------------------------------------#
+  
 CREATE TABLE planes_evaluacion(
-  id_plan BIGINT NOT NULL PRIMARY KEY,
+  id_plan serial NOT NULL PRIMARY KEY,
   cantidad_notas BIGINT,
   descripcion_plan varchar(200),
   id_grupo BIGINT NOT NULL
 );
 
+ALTER TABLE planes_evaluacion
+	ADD CONSTRAINT fk_planes_grupos
+	FOREIGN KEY (id_grupo)
+	REFERENCES grupos(id_grupo)
+
+
+
+#--------------------------------------------------------------------------------------------------#
+
+CREATE TABLE notas(
+	id_nota SERIAL NOT NULL PRIMARY KEY,
+	valor varchar(5),
+	id_plan_evaluacion BIGINT NOT NULL
+);
+
+ALTER TABLE notas
+	ADD CONSTRAINT fk_notas_planes
+	FOREIGN KEY (id_plan_evaluacion)
+	REFERENCES planes_evaluacion (id_plan)
+
+
+#--------------------------------------------------------------------------------------------------#
 
 
 
