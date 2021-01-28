50df900a937cf84f32f75d81c664d276d8b2f032
IzzJuan <juangarciavxx@gmail.com>
2021-01-28 18:27:51 -0500

    creacion ruta signin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,6 +12,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(require('./routes/index.routes'))
+app.use(require('./routes/index.routes'));
+app.use(require('./routes/autentication.routes'));
 
 module.exports = app;
--- Backend/src/routes/autentication.routes.js
+++ Backend/src/routes/autentication.routes.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../database');
+const format = require('pg-format');
+
+router.post('/signin', async (req, res) => {
+
+    const { tipo_documento, documento_usuario, nombre_usuario, apellido_usuario, genero, fecha_nacimiento,
+        direccion_residencia, ciudad_residencia, telefono_residencia, correo_electronico, telefono_celular,
+        foto_estudiante, copia_documento } = req.body;
+
+    let id = await pool.query('SELECT (SELECT id_usuarios FROM usuarios  ORDER BY id_usuarios DESC LIMIT 1) + 1 AS id_usuarios;');
+    const codigo_usuario = new Date().getFullYear() + ('00' + id.rows[0]['id_usuarios']).slice(-3);
+
+    const newUser = [
+        6, codigo_usuario, tipo_documento, documento_usuario, nombre_usuario, apellido_usuario, genero, fecha_nacimiento,
+        direccion_residencia, ciudad_residencia, telefono_residencia, correo_electronico, telefono_celular,
+        foto_estudiante, copia_documento
+    ];
+
+    const rows = await pool.query(format(`INSERT INTO usuarios (id_usuarios, codigo_usuario, tipo_documento, documento_usuario, nombres_usuario, apellidos_usuario, genero, fecha_nacimiento,
+        direccion_residencia, ciudad_residencia, telefono_residencia, correo_electronico, telefono_celular,
+        foto_estudiante, copia_documento) VALUES %L`, [newUser]));
+});
 
 
 
