1c278feaa3800887a84f3c256ef97c8b5d3efd04
JuanPabloRiveraAlvarezINEM <juanpariveraalvarez@gmail.com>
2021-01-28 19:01:09 -0500

    Agregamos todas las trabla el auntoincremento

--- SQL/base_datos.sql
+++ SQL/base_datos.sql
@@ -1,14 +1,15 @@
-CREATE DATABASE academia_geek;
-
 CREATE TYPE TIPO_DOCUMENTO AS ENUM('TI','NUIM','CC');
+CREATE TYPE ROL AS ENUM('ESTUDIANTE','DOCENTE','ADMINISTRATIVO');
+CREATE TYPE GENERO AS ENUM('MASCULINO','FEMENINO','OTRO');
 CREATE TABLE usuarios(
   id_usuario serial NOT NULL PRIMARY KEY,
+	rol ROL,
   codigo_usuario varchar(10),
   tipo_documento TIPO_DOCUMENTO,
   documento_usuario varchar(15),
   nombre_usuario varchar(30),
   apellido_usuario varchar(30),
-  sexo BIGINT,
+  genero GENERO,
   fecha_nacimiento date,
   direccion_residencia varchar(20),
   ciudad_residencia varchar(20),
@@ -19,16 +20,14 @@ CREATE TABLE usuarios(
   copia_documento varchar(500));
 
 
-#--------------------------------------------------------------------------------------------------#
 
 CREATE TYPE JORNADA AS ENUM('mañana','tarde');
 CREATE TABLE grupos(
   id_grupo serial NOT NULL PRIMARY KEY,
   codigo_grupo varchar(10),
-  jornada JORNADA);
+  jornada JORNADA);	
 
 
-#--------------------------------------------------------------------------------------------------#
 
 CREATE TABLE usuarios_grupos(
     id_usuarios_grupo serial NOT NULL PRIMARY KEY,
@@ -43,10 +42,9 @@ ALTER TABLE usuarios_grupos
 ALTER TABLE usuarios_grupos
   ADD CONSTRAINT fk_usuarios_grupos_u
   FOREIGN KEY (id_usuario)
-  REFERENCES usuarios (id_usuario)
+  REFERENCES usuarios (id_usuario);
+	
 
-
-#--------------------------------------------------------------------------------------------------#
 
 CREATE TABLE materias(
   id_materia serial NOT NULL PRIMARY KEY,
@@ -67,12 +65,12 @@ ALTER TABLE materias
   REFERENCES grupos (id_grupo); 
 
 
-#--------------------------------------------------------------------------------------------------#
-  
+
 CREATE TABLE planes_evaluacion(
-  id_plan serial NOT NULL PRIMARY KEY,
+  id_plan SERIAL NOT NULL PRIMARY KEY,
   cantidad_notas BIGINT,
   descripcion_plan varchar(200),
+	id_materia BIGINT NOT NULL,
   id_grupo BIGINT NOT NULL
 );
 
@@ -80,10 +78,13 @@ ALTER TABLE planes_evaluacion
 	ADD CONSTRAINT fk_planes_grupos
 	FOREIGN KEY (id_grupo)
 	REFERENCES grupos(id_grupo)
+	
+ALTER TABLE planes_evaluacion
+	ADD CONSTRAINT fk_planes_materias
+	FOREIGN KEY (id_materia)
+	REFERENCES materias(id_materia)
+	
 
-
-
-#--------------------------------------------------------------------------------------------------#
 
 CREATE TABLE notas(
 	id_nota SERIAL NOT NULL PRIMARY KEY,
@@ -96,10 +97,19 @@ ALTER TABLE notas
 	FOREIGN KEY (id_plan_evaluacion)
 	REFERENCES planes_evaluacion (id_plan)
 
+CREATE TYPE ESTADO AS ENUM('1','2','3');
+CREATE TABLE historial(
+ id_historial SERIAL NOT NULL PRIMARY KEY,
+ codigo_estudiante varchar(10),
+ ano varchar(5),
+ grado varchar(3),
+ estado ESTADO,
+ nota_promedio varchar(5),
+ id_usuario	BIGINT NOT NULL
+);
 
-#--------------------------------------------------------------------------------------------------#
 
-
-
-
-
+ALTER TABLE historial
+ADD CONSTRAINT fk_codigo
+FOREIGN KEY (id_usuario)
+REFERENCES usuarios (id_usuario)
