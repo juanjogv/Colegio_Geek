7f0d8b0bf0204bc5c00e5a512723d0b86227f791
IzzJuan <juangarciavxx@gmail.com>
2021-01-28 23:06:21 -0500

    comenzando ruta de login

--- Backend/src/routes/autentication.routes.js
+++ Backend/src/routes/autentication.routes.js
@@ -5,24 +5,33 @@ const format = require('pg-format');
 
 router.post('/signin', async (req, res) => {
 
-    const { tipo_documento, documento_usuario, nombre_usuario, apellido_usuario, genero, fecha_nacimiento,
+    const { rol, tipo_documento, documento_usuario, nombre_usuario, apellido_usuario, genero, fecha_nacimiento,
         direccion_residencia, ciudad_residencia, telefono_residencia, correo_electronico, telefono_celular,
-        foto_estudiante, copia_documento } = req.body;
+        foto_estudiante, copia_documento, contrasena } = req.body;
 
     let id = await pool.query('SELECT (SELECT id_usuarios FROM usuarios  ORDER BY id_usuarios DESC LIMIT 1) + 1 AS id_usuarios;');
     const codigo_usuario = new Date().getFullYear() + ('00' + id.rows[0]['id_usuarios']).slice(-3);
 
     const newUser = [
-        6, codigo_usuario, tipo_documento, documento_usuario, nombre_usuario, apellido_usuario, genero, fecha_nacimiento,
+        rol, codigo_usuario, tipo_documento, documento_usuario, nombre_usuario, apellido_usuario, genero, fecha_nacimiento,
         direccion_residencia, ciudad_residencia, telefono_residencia, correo_electronico, telefono_celular,
-        foto_estudiante, copia_documento
+        foto_estudiante, copia_documento, contrasena
     ];
 
-    const rows = await pool.query(format(`INSERT INTO usuarios (id_usuarios, codigo_usuario, tipo_documento, documento_usuario, nombres_usuario, apellidos_usuario, genero, fecha_nacimiento,
+    const rows = await pool.query(format(`INSERT INTO usuarios (codigo_usuario, tipo_documento, documento_usuario, nombres_usuario, apellidos_usuario, genero, fecha_nacimiento,
         direccion_residencia, ciudad_residencia, telefono_residencia, correo_electronico, telefono_celular,
         foto_estudiante, copia_documento) VALUES %L`, [newUser]));
 });
 
+router.post('/login', async (req, res) => {
+    const { codigo_usuario, documento_usuario, contrasena } = req.body;
+    const checkUser = [
+        codigo_usuario,
+        documento_usuario,
+        contrasena
+    ]
+
+});
 
 
 module.exports = router;
