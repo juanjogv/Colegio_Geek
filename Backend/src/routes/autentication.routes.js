04052f88cb87f5addfecc3768d631022afa46df2
IzzJuan <juangarciavxx@gmail.com>
2021-01-29 19:24:59 -0500

    mejoras en la ruta de sigin

--- Backend/src/routes/autentication.routes.js
+++ Backend/src/routes/autentication.routes.js
@@ -2,34 +2,41 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database');
 const format = require('pg-format');
+const helpers = require('../lib/helpers');
 
 router.post('/signin', async (req, res) => {
 
     const { rol, tipo_documento, documento_usuario, nombre_usuario, apellido_usuario, genero, fecha_nacimiento,
         direccion_residencia, ciudad_residencia, telefono_residencia, correo_electronico, telefono_celular,
-        foto_estudiante, copia_documento, contrasena } = req.body;
+        foto_estudiante, copia_documento } = req.body;
 
-    let id = await pool.query('SELECT (SELECT id_usuarios FROM usuarios  ORDER BY id_usuarios DESC LIMIT 1) + 1 AS id_usuarios;');
-    const codigo_usuario = new Date().getFullYear() + ('00' + id.rows[0]['id_usuarios']).slice(-3);
+    const pass_usuario = await helpers.encryptPassword(documento_usuario);
+    const codigo_usuario = await helpers.createCodigoUsuario();
 
     const newUser = [
         rol, codigo_usuario, tipo_documento, documento_usuario, nombre_usuario, apellido_usuario, genero, fecha_nacimiento,
         direccion_residencia, ciudad_residencia, telefono_residencia, correo_electronico, telefono_celular,
-        foto_estudiante, copia_documento, contrasena
+        foto_estudiante, copia_documento, pass_usuario
     ];
+    const rows = await pool.query(format(`INSERT INTO usuarios (rol, codigo_usuario, tipo_documento, documento_usuario, nombre_usuario, apellido_usuario, genero, fecha_nacimiento,
+    direccion_residencia, ciudad_residencia, telefono_residencia, correo_electronico, telefono_celular,
+    foto_estudiante, copia_documento, pass_usuario) VALUES %L`, [newUser]));
 
-    const rows = await pool.query(format(`INSERT INTO usuarios (codigo_usuario, tipo_documento, documento_usuario, nombres_usuario, apellidos_usuario, genero, fecha_nacimiento,
-        direccion_residencia, ciudad_residencia, telefono_residencia, correo_electronico, telefono_celular,
-        foto_estudiante, copia_documento) VALUES %L`, [newUser]));
 });
 
-router.post('/login', async (req, res) => {
-    const { codigo_usuario, documento_usuario, contrasena } = req.body;
-    const checkUser = [
-        codigo_usuario,
-        documento_usuario,
-        contrasena
-    ]
+router.get('/login/:codigo_usuario/:contrasena', async (req, res) => {
+    const { codigo_usuario, contrasena } = req.body;
+
+    const rows = await pool.query(`SELECT * FROM usuarios WHERE codigo_usuario = '${codigo_usuario}'`);
+
+    if (rows.rows.length > 0) {
+
+        const savedpass = rows.rows[0].contrasena_usuario;
+        const validPass = await helpers.matchPassword(userpass, savedpass);
+        rows.push({ validPass: validPass })
+        res.json(rows);
+
+    }
 
 });
 
