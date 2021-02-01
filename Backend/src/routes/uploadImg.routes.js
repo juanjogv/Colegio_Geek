a72e196dd7e73d70961816cffd95043b1354c360
dubis78 <dubis85201@hotmail.com>
2021-02-01 01:04:16 -0500

    testing GCP Bucket

--- Backend/src/routes/uploadImg.routes.js
+++ Backend/src/routes/uploadImg.routes.js
@@ -1,42 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Multer = require("multer")
-const { Storage } = require("@google-cloud/storage")
-const uuid = require("uuid");
-const uuidv1 = uuid.v1;
+const multer = require("multer");
+const { v4: uuidv4 } = require('uuid');
+const path = require("path");
+const { Storage } = require("@google-cloud/storage");
 
 const storage = new Storage({
+    // keyFilename: path.join(__dirname, "../backend-303114-5f186b208c6c.json"),
     projectId: process.env.GCLOUD_PROJECT,
-    credentials: {
-        client_email: process.env.GCLOUD_CLIENT_EMAIL,
-        private_key: process.env.GCLOUD_PRIVATE_KEY
+    credentials:{client_email:process.env.GCS_CLIEN_EMAIL,
+    private_key:process.env.GCLOUD_PRIVATE_KEY}
+    // filename:
+  });
+  
+
+  const bucket = storage.bucket(process.env.GCS_BUCKET);
+
+  let uploadHandler = multer({
+    storage: multer.memoryStorage(),
+    limits:{ 
+      fileZise: 5 * 1024 * 1024
     }
-})
+  });
 
-const multer = Multer({
-    storage: Multer.memoryStorage(),
-    limits: {
-        fileSize: 5 * 1024 * 1024
-    }
-})
-
-const bucket = storage.bucket(process.env.GCS_BUCKET);
-
-router.post('/imageupload', multer.single('file'), (req, res) => {
-    const newFileName = uuidv1() + "-" + req.file.originalname
-    const blob = bucket.file(newFileName)
-    const blobStream = blob.createWriteStream({
-        resumable: false,
-        gzip: true
-    }).on('finish', () => {
-        const publicUrl = `https://storage.googleapis.com/${process.env.GCS_BUCKET}/${blob.name}`
-
-        const imageDetails = JSON.parse(req.body.data)
-        imageDetails.image = publicUrl
-        //db.Image.create(imageDetails).then(() => res.json(imageDetails))
-    })
-    blobStream.end(req.file.buffer)
-    res.json('ok')
-})
+  router.post('/imageupload',uploadHandler.single('file'),(req, res)=>{  
+    console.log(req.file);
+    const newFileName= uuidv4()+path.extname(req.file.originalname);
+    const blob = bucket.file(newFileName);
+    const blobStream=blob.createWriteStream({
+    resumable: false,
+    gzip:true
+    }).on('finish',()=>{
+      const publicURL=`https://storage.googleapis.com/${process.env.GCS_BUCKET}/${blob.name}`
+      //const imageDetails=JSON.parse(req.body);
+      imageDetails.image=publicURL;res.json('ok');
+      //db.Image.create(imageDetails).then(() => res.json(imageDetails))
+      });
+    blobStream.end(req.file.buffer);
+    res.json('ok');
+  });
 
 module.exports = router;
