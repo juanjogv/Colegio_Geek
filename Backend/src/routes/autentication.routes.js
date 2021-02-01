22f049111846601774e63c3deaf787d60a55d801
IzzJuan <juangarciavxx@gmail.com>
2021-01-31 22:52:26 -0500

    implementacion de buckets para subir imagenes y mejoras en la validacion de usuario

--- Backend/src/routes/autentication.routes.js
+++ Backend/src/routes/autentication.routes.js
@@ -6,10 +6,10 @@ const helpers = require('../lib/helpers');
 
 router.post('/signin', async (req, res) => {
     console.log(req.body)
-    const {rol, tipo_documento, documento_usuario, nombre_usuario, apellido_usuario, genero, fecha_nacimiento, ciudad_residencia, 
-        direccion_residencia, telefono_residencia, correo_electronico, telefono_celular,foto_estudiante, copia_documento} = req.body;
-        console.log('holis',rol, tipo_documento, documento_usuario, nombre_usuario, apellido_usuario, genero, ciudad_residencia, 
-        direccion_residencia, telefono_residencia, correo_electronico, telefono_celular,foto_estudiante, copia_documento);
+    const { rol, tipo_documento, documento_usuario, nombre_usuario, apellido_usuario, genero, fecha_nacimiento, ciudad_residencia,
+        direccion_residencia, telefono_residencia, correo_electronico, telefono_celular, foto_estudiante, copia_documento } = req.body;
+    console.log('holis', rol, tipo_documento, documento_usuario, nombre_usuario, apellido_usuario, genero, ciudad_residencia,
+        direccion_residencia, telefono_residencia, correo_electronico, telefono_celular, foto_estudiante, copia_documento);
     const pass_usuario = await helpers.encryptPassword(documento_usuario);
     const codigo_usuario = await helpers.createCodigoUsuario();
 
@@ -24,18 +24,11 @@ router.post('/signin', async (req, res) => {
 
 });
 
-<<<<<<< HEAD
 router.post('/login', async (req, res) => {
     const { correo_electronico, pass } = req.body;
 
     const { rows } = await pool.query(`SELECT * FROM usuarios WHERE correo_electronico = '${correo_electronico}'`);
     if (rows.length > 0) {
-=======
-router.get('/login/:codigo_usuario/:contrasena', async (req, res) => {
-    const { correo_electronico, contrasena } = req.body;
-    console.log(correo_electronico, contrasena);
-    const rows = await pool.query(`SELECT * FROM usuarios WHERE correo_electronico = '${correo_electronico}'`);
->>>>>>> b07dcda6e287288bfe78fe8423853a468bfe9dce
 
         const savedpass = rows[0].pass_usuario;
         const validPass = await helpers.matchPassword(pass, savedpass);
@@ -45,24 +38,6 @@ router.get('/login/:codigo_usuario/:contrasena', async (req, res) => {
     }
 
 });
-
-
-router.post('/login', async (req, res) => {
-    const { correo_electronico, contrasena } = req.body;
-    console.log(correo_electronico, contrasena);
-    const rows = await pool.query(`SELECT * FROM usuarios WHERE correo_electronico = '${correo_electronico}'`);
-
-    if (rows.rows.length > 0) {
-
-        const savedpass = rows.rows[0].contrasena_usuario;
-        const validPass = await helpers.matchPassword(contrasena, savedpass);
-        rows.push({ validPass: validPass })
-        res.json(rows);
-
-    }
-
-});
-
 
 
 module.exports = router;
