b3c876c8781495b095dbe7794d7966e1d17cbe5e
IzzJuan <juangarciavxx@gmail.com>
2021-02-02 14:34:13 -0500

    funcion para subir imagenes

--- Backend/src/routes/autentication.routes.js
+++ Backend/src/routes/autentication.routes.js
@@ -5,14 +5,8 @@ const format = require('pg-format');
 const helpers = require('../lib/helpers');
 
 router.post('/signin', async (req, res) => {
-<<<<<<< HEAD
     const { rol, tipo_documento, documento_usuario, nombre_usuario, apellido_usuario, genero, fecha_nacimiento, ciudad_residencia,
         direccion_residencia, telefono_residencia, correo_electronico, telefono_celular, foto_estudiante, copia_documento } = req.body;
-=======
-    console.log(req.body)
-    const {rol, tipo_documento, documento_usuario, nombre_usuario, apellido_usuario, genero, fecha_nacimiento, ciudad_residencia, 
-        direccion_residencia, telefono_residencia, correo_electronico, telefono_celular,foto_estudiante, copia_documento} = req.body;
->>>>>>> a12f5ee7693b5bbd4ca7c49ac3a5fbd86e105468
     const pass_usuario = await helpers.encryptPassword(documento_usuario);
     const codigo_usuario = await helpers.createCodigoUsuario();
 
@@ -25,41 +19,18 @@ router.post('/signin', async (req, res) => {
         const rows = await pool.query(format(`INSERT INTO usuarios (rol, codigo_usuario, tipo_documento, documento_usuario, nombre_usuario, apellido_usuario, genero, fecha_nacimiento,
             direccion_residencia, ciudad_residencia, telefono_residencia, correo_electronico, telefono_celular,
             foto_estudiante, copia_documento, pass_usuario) VALUES %L`, [newUser]));
-            res.status(201).json("Usuario registrado");
+        res.status(201).json("Usuario registrado");
     } catch (error) {
         res.json("Error");
     }
-    
+
 
 });
-
-<<<<<<< HEAD
-router.post('/login', async (req, res) => {
-    const { email, pass } = req.body;
-
-    const { rows } = await pool.query(`SELECT * FROM usuarios WHERE correo_electronico = '${email}'`);
-=======
-
-// router.get('/login/:codigo_usuario/:contrasena', async (req, res) => {
-//     const { correo_electronico, contrasena } = req.body;
-//     console.log(correo_electronico, contrasena);
-//     const rows = await pool.query(`SELECT * FROM usuarios WHERE correo_electronico = '${correo_electronico}'`);
-
-//     if (rows.rows.length > 0) {
-
-//         const savedpass = rows.rows[0].contrasena_usuario;
-//         const validPass = await helpers.matchPassword(userpass, savedpass);
-//         rows.push({ validPass: validPass })
-//         res.json(rows);
-//     }
-// });
-
 
 router.post('/login', async (req, res) => {
     const { correo_electronico, contrasena_usuario } = req.body;
     console.log(req.body)
     const { rows } = await pool.query(`SELECT * FROM usuarios WHERE correo_electronico = '${correo_electronico}'`);
->>>>>>> a12f5ee7693b5bbd4ca7c49ac3a5fbd86e105468
     if (rows.length > 0) {
         const savedpass = rows[0].pass_usuario;
         const validPass = await helpers.matchPassword(contrasena_usuario, savedpass);
