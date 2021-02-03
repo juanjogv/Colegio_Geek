ad034b0fd9f2f776168e7148c4f88090414a9212
IzzJuan <juangarciavxx@gmail.com>
2021-02-02 22:05:18 -0500

    cambios en subir imagenes

--- Backend/src/routes/uploadImg.routes.js
+++ Backend/src/routes/uploadImg.routes.js
@@ -1,26 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const Multer = require("multer");
+const Multer = require('multer');
 const { Storage } = require("@google-cloud/storage");
-const uuid = require("uuid");
-const uuidv1 = uuid.v1;
 const { UploadToBucket } = require('../lib/UploadToBucket');
 
 const storage = new Storage({
-    // keyFilename: path.join(__dirname, "../backend-303114-5f186b208c6c.json"),
     projectId: process.env.GCLOUD_PROJECT,
     credentials: {
-        client_email: process.env.GCS_CLIEN_EMAIL,
+        client_email: process.env.GCLOUD_CLIENT_EMAIL,
         private_key: process.env.GCLOUD_PRIVATE_KEY
     }
-    // filename:
-});
+})
+
+const multer = Multer({
+    storage: Multer.memoryStorage(),
+    limits: {
+        fileSize: 5 * 1024 * 1024
+    }
+})
 
 
 const bucket = storage.bucket(process.env.GCS_BUCKET);
 
-router.post('/imageupload', multer.single('file'), (req, res) => {
-    UploadToBucket(req);
+router.post('/imageupload', multer.single('file'), async (req, res) => {
+    UploadToBucket(req)
     res.json('ok');
 })
 
