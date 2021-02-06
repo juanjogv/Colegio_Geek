5332950873661ee182f6ba31925c4d543986ff0d
IzzJuan <juangarciavxx@gmail.com>
2021-02-06 15:49:44 -0500

    cambios para preparar el deploy

--- Backend/src/routes/index.routes.js
+++ Backend/src/routes/index.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path')
 
 router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 module.exports = router;
