8e0d8c61b77ff5b09c8388361b5cee9179b8c1ea
IzzJuan <juangarciavxx@gmail.com>
2021-02-06 13:15:33 -0500

    cambios para preparar el deploy

--- Backend/src/routes/index.routes.js
+++ Backend/src/routes/index.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path')
 
-router.get('/*', function (req, res) {
+router.get('/', function (req, res) {
     res.send('API funcionando!')
 });
 
